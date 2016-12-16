deda5a7037b4557e00f6ddd953fde1eef99f2ac1
Sean Lee <freshdried@gmail.com>
2016-12-16 16:22:05 -0500

    babelized for safari compatibility

--- index.js
+++ index.js
@@ -1,64 +1,65 @@
-/* @flow */
-
-/*::
-
-// from react
-type Component = Object
+"use strict";
 
 // from react-router
-type IndexRoute = {component : Component}
-
-// from react-router
-type Route = {
-  path : string,
-  component : Component,
-  childRoutes? : Array<Route>,
-  indexRoute? : IndexRoute
-}
-
-*/
-
-const R /*:
-(
-  path : string, 
-  component : Component, 
-  ...transformers : Array<(route : Route) => Route>
-) 
-  => Route 
-*/
-= (path, component, ...transformers) => { let route = {path, component};
-  for (let transform of transformers) {
-    route = transform(route);
+var R = function R(path, component) {
+  for (var _len = arguments.length, transformers = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    transformers[_key - 2] = arguments[_key];
   }
+
+  var route = { path: path, component: component };
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = transformers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var transform = _step.value;
+
+      route = transform(route);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
   return route;
 };
 
+// from react-router
 
-const index /*:
-(component : Component) 
-  => (r : Route) 
-  => Route 
-*/
-= component => function(r) {
-  if ("indexRoute" in r) {
-    console.error("indexRoute already exists!", r);
-  }
-  Object.assign(r, {indexRoute: {component}});
-  return r;
+
+// from react
+
+
+var index = function index(component) {
+  return function (r) {
+    if ("indexRoute" in r) {
+      console.error("indexRoute already exists!", r);
+    }
+    Object.assign(r, { indexRoute: { component: component } });
+    return r;
+  };
 };
 
-const child /*:
-(childRoute : Route) 
-  => (r : Route) 
-  => Route 
-*/
-= childRoute => function (r) {
-  if (!Array.isArray(r.childRoutes)) {
-    r.childRoutes = [];
-  }
-  r.childRoutes.push(childRoute);
-  return r;
+var child = function child(childRoute) {
+  return function (r) {
+    if (!Array.isArray(r.childRoutes)) {
+      r.childRoutes = [];
+    }
+    r.childRoutes.push(childRoute);
+    return r;
+  };
 };
 
+module.exports = { R: R, index: index, child: child };
 
-module.exports = {R, index, child};
---
+++ index_src.js
@@ -0,0 +1,56 @@
+/* @flow */
+
+
+type Component = Object
+
+type IndexRoute = {component : Component}
+
+type Route = {
+  path : string,
+  component : Component,
+  childRoutes? : Array<Route>,
+  indexRoute? : IndexRoute
+}
+
+
+const R :
+(
+  path : string, 
+  component : Component, 
+  ...transformers : Array<(route : Route) => Route>
+) 
+  => Route 
+= (path, component, ...transformers) => { let route = {path, component};
+  for (let transform of transformers) {
+    route = transform(route);
+  }
+  return route;
+};
+
+
+const index :
+(component : Component) 
+  => (r : Route) 
+  => Route 
+= component => function(r) {
+  if ("indexRoute" in r) {
+    console.error("indexRoute already exists!", r);
+  }
+  Object.assign(r, {indexRoute: {component}});
+  return r;
+};
+
+const child :
+(childRoute : Route) 
+  => (r : Route) 
+  => Route 
+= childRoute => function (r) {
+  if (!Array.isArray(r.childRoutes)) {
+    r.childRoutes = [];
+  }
+  r.childRoutes.push(childRoute);
+  return r;
+};
+
+
+module.exports = {R, index, child};
